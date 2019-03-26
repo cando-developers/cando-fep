6092aa764686d568af2509d535f7bbf5f3510a3d
Chris Schafmeister <meister@temple.edu>
2019-03-26 11:45:26 -0400

    Update setenv.sh
    This is only good for shiho and chris

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,1 +1,1 @@
-export PATH=~/Development/cando/build/boehm:$AMBERHOME/bin:~/quicklisp/local-projects/fep/bin:$PATH
+export PATH=~/Development/cando/build/boehm:$AMBERHOME/bin:~/Development/cando/src/lisp/modules/quicklisp/local-projects/fep/bin:$PATH
